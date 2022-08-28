4dde51d2cbf9af6b7cdc0460b272ce901fcd1e5a
sinulingga23 <sinulinggatwo@gmail.com>
2022-08-28 17:37:47 +0700

    feat: create the connectionn to mongodb database

---
+++ config/database.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func ConnectToMongoDb() (*mongo.Database, error) {
+	mongoDbUser := os.Getenv("MONGO_DB_USER")
+	mongoDbPassword := os.Getenv("MONGO_DB_PASSWORD")
+	mongoDbHost := os.Getenv("MONGO_DB_HOST")
+	mongoDbPort := os.Getenv("MONGO_DB_PORT")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	
+	mongoUri := fmt.Sprintf("mongodb://%s:%s@%s:%s", mongoDbUser, mongoDbPassword, mongoDbHost, mongoDbPort)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+	if err != nil {
+		return nil, err
+	}
+
+	database := client.Database("go_pos")
+	if err := database.Client().Ping(ctx, readpref.Primary()); err != nil {
+	}
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	log.Printf("[DATABASE]: Connect successfully to MongoDB!\n")
+	return database, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+
+	"github.com/sinulingga23/go-pos/config"
+)
+
 func main() {
-	
+	_, err := config.ConnectToMongoDb()
+	if err != nil {
+		log.Printf("[DATABASE]: %s\b", err.Error())
+	}
 }
