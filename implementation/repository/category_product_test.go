5e6dedc4d16c5f389aca1a4b11b34f934de4e915
sinulingga23 <sinulinggatwo@gmail.com>
2022-09-08 23:48:57 +0700

    feat: test the method categoryProductRepository.findById

--- implementation/repository/category_product_test.go
+++ implementation/repository/category_product_test.go
@@ -16,25 +16,26 @@ func TestCategoryProductRepository_Create_Success(t *testing.T) {
 	ctx := context.TODO()
 	database, err := config.ConnectToMongoDb(ctx)
 	if err != nil {
-		t.Fatalf(err.Error())
+		t.Fatal(err.Error())
 	}
+	
 	_, err = database.Collection(CategoryProductCollection).DeleteMany(ctx, struct {}{})
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Fatal(err.Error())
 	}
 
-	categoryRepository := NewCategoryProductRepository(database)
+	categoryProductRepository := NewCategoryProductRepository(database)
 
 	wantCategoryName := "Elektronik"
 	wantDescription := "Menjual berbagai macam barang elektronik"
 	
-	cretaedCategoryProduct, err := categoryRepository.Create(ctx, domain.CategoryProduct{
+	cretaedCategoryProduct, err := categoryProductRepository.Create(ctx, domain.CategoryProduct{
 		Id: primitive.NewObjectID(),
 		CategoryName: "Elektronik",
 		Description: "Menjual berbagai macam barang elektronik",
 	})
 	if err != nil {
-		t.Fatalf(err.Error())
+		t.Fatal(err.Error())
 	}
 
 	if strings.Compare(wantCategoryName, cretaedCategoryProduct.CategoryName) != 0 {
@@ -43,5 +44,50 @@ func TestCategoryProductRepository_Create_Success(t *testing.T) {
 
 	if strings.Compare(wantDescription, cretaedCategoryProduct.Description)  != 0 {
 		t.Fatalf("got %q want %q\n", cretaedCategoryProduct.Description, wantDescription)
+	}
+}
+
+func TestCategoryProductRepository_FindById_Exists(t *testing.T) {
+	ctx := context.TODO()
+	database, err := config.ConnectToMongoDb(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	
+	_, err = database.Collection(CategoryProductCollection).DeleteMany(ctx, struct {}{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	categoryProductRepository := NewCategoryProductRepository(database)
+
+	wantId := primitive.NewObjectID()
+	wantCategoryName := "Kesehatan"
+	wantDescription := "Menyediakan berbagai produk kesehatan."
+
+	createdCategoryProduct, err := categoryProductRepository.Create(ctx, domain.CategoryProduct{
+		Id: wantId,
+		CategoryName: "Kesehatan",
+		Description: "Menyediakan berbagai produk kesehatan.",
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	currentCategoryProduct, err := categoryProductRepository.FindById(ctx, createdCategoryProduct.Id)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if wantId != currentCategoryProduct.Id {
+		log.Fatalf("got %q want %q\n", currentCategoryProduct.Id, wantId)
+	}
+
+	if strings.Compare(wantCategoryName, currentCategoryProduct.CategoryName) != 0 {
+		log.Fatalf("got %q want %q\n", currentCategoryProduct.CategoryName, wantCategoryName)
+	}
+
+	if strings.Compare(wantDescription, currentCategoryProduct.Description) != 0 {
+		log.Fatalf("got %q want %q\n", wantDescription, currentCategoryProduct.Description)
 	}
 }
