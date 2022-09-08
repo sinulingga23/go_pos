9ec95cdfd7326e8438565cc29de79d466c54588c
sinulingga23 <sinulinggatwo@gmail.com>
2022-09-09 00:09:22 +0700

    feat: ensure the return of error is expected as is

--- implementation/repository/category_product_test.go
+++ implementation/repository/category_product_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/sinulingga23/go-pos/config"
 	"github.com/sinulingga23/go-pos/domain"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 
@@ -89,5 +90,43 @@ func TestCategoryProductRepository_FindById_Exists(t *testing.T) {
 
 	if strings.Compare(wantDescription, currentCategoryProduct.Description) != 0 {
 		log.Fatalf("got %q want %q\n", wantDescription, currentCategoryProduct.Description)
+	}
+}
+
+func TestCategoryProductRepository_FindById_NotExists(t *testing.T) {
+	ctx := context.TODO()
+	database, err := config.ConnectToMongoDb(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	_, err = database.Collection(CategoryProductCollection).DeleteMany(ctx, struct {}{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	wantError1 := mongo.ErrNoDocuments
+	wantError2 := mongo.ErrNoDocuments
+
+	categoryProductRepository := NewCategoryProductRepository(database)
+
+	_, err = categoryProductRepository.Create(ctx, domain.CategoryProduct{
+		Id: primitive.NewObjectID(),
+		CategoryName: "Hiburan",
+		Description: "Semua kebutuhan hiburanmu ada disini!.",
+	})
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	_, err1 := categoryProductRepository.FindById(ctx, primitive.NewObjectID())
+	_, err2 := categoryProductRepository.FindById(ctx, primitive.ObjectID{})
+
+	if wantError1 != err1 {
+		log.Fatalf("got %q want %q\n", err1.Error(), wantError1.Error())
+	}
+
+	if wantError2 != err2 {
+		log.Fatalf("got %q want %q\n", err2.Error(), wantError2.Error())
 	}
 }
