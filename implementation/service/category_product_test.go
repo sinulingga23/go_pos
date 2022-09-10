a3a7fdff3b15a4e9b9fcc62bbf43e3b829c82be5
sinulingga23 <sinulinggatwo@gmail.com>
2022-09-10 13:19:13 +0700

    feat: di integration test for CategoryProdutService.Create method

--- implementation/service/category_product_test.go
+++ implementation/service/category_product_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/sinulingga23/go-pos/config"
+	"github.com/sinulingga23/go-pos/definition"
 	"github.com/sinulingga23/go-pos/implementation/repository"
 	"github.com/sinulingga23/go-pos/payload"
 )
@@ -44,5 +45,79 @@ func TestCategoryProductService_Create_Success(t *testing.T) {
 
 	if strings.Compare(wantDescription, createdCategoryProduct.Description) != 0 {
 		log.Fatalf("got %q want %q\n", createdCategoryProduct.Description, wantDescription)
+	}
+}
+
+func TestCategoryProductService_Create_TheFieldsIsEmpty(t *testing.T) {
+	ctx := context.TODO()
+	database, err := config.ConnectToMongoDb(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	_, err = database.Collection(repository.CategoryProductCollection).DeleteMany(ctx, struct {}{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	wantError1 := definition.ErrBadRequest
+	wantError2 := definition.ErrBadRequest
+
+	categoryProductRepository := repository.NewCategoryProductRepository(database)
+	categoryProductService := NewCategoryProductService(categoryProductRepository)
+
+	_, err1 :=  categoryProductService.Create(payload.CreateCategoryProductRequest{
+		CategoryName: "",
+		Description: "",
+	})
+
+	_, err2 := categoryProductService.Create(payload.CreateCategoryProductRequest{
+		CategoryName: "   ",
+		Description: "       ",
+	})
+
+	if wantError1 != err1 {
+		log.Fatalf("got %q want %q\n", err1.Error(), wantError1.Error())
+	}
+
+	if wantError2 != err2 {
+		log.Fatalf("got %q want %q\n", err2.Error(), wantError2.Error())
+	}
+}
+
+func TestCategoryProductService_Create_IsThereFieldsEmptyAndNot(t *testing.T) {
+	ctx := context.TODO()
+	database, err := config.ConnectToMongoDb(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	_, err = database.Collection(repository.CategoryProductCollection).DeleteMany(ctx, struct {}{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	wantError1 := definition.ErrBadRequest
+	wantError2 := definition.ErrBadRequest
+
+	categoryProductRepository := repository.NewCategoryProductRepository(database)
+	categoryProductService := NewCategoryProductService(categoryProductRepository)
+
+	_, err1 :=  categoryProductService.Create(payload.CreateCategoryProductRequest{
+		CategoryName: "",
+		Description: "Menyediakan semua kebutuhan hiburanmu!.",
+	})
+
+	_, err2 := categoryProductService.Create(payload.CreateCategoryProductRequest{
+		CategoryName: "Hiburan",
+		Description: "",
+	})
+
+	if wantError1 != err1 {
+		log.Fatalf("got %q want %q\n", err1.Error(), wantError1.Error())
+	}
+
+	if wantError2 != err2 {
+		log.Fatalf("got %q want %q\n", err2.Error(), wantError2.Error())
 	}
 }
