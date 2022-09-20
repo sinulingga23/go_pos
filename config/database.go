bede2502c21895d97c142367189b114426b17985
sinulingga23 <sinulinggatwo@gmail.com>
2022-09-20 09:40:17 +0700

    update

--- config/database.go
+++ config/database.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"context"
-	"fmt"
 	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -11,17 +10,14 @@ import (
 )
 
 func ConnectToMongoDb(ctx context.Context) (*mongo.Database, error) {
-	mongoDbUser := os.Getenv("MONGO_DB_USER")
-	mongoDbPassword := os.Getenv("MONGO_DB_PASSWORD")
-	mongoDbCluster := os.Getenv("MONGO_DB_CLUSTER")
-	mongoDbName := os.Getenv("MONGO_DB_NAME")
-	
-	mongoUri := fmt.Sprintf("mongodb+srv://%s:%s@%s/?retryWrites=true&w=majority", mongoDbUser, mongoDbPassword, mongoDbCluster)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+
+	mongoDbUri := os.Getenv("MONGO_DB_URI")
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoDbUri))
 	if err != nil {
 		return nil, err
 	}
 
+	mongoDbName := os.Getenv("MONGO_DB_NAME")
 	database := client.Database(mongoDbName)
 	if err := database.Client().Ping(ctx, readpref.Primary()); err != nil {
 		return nil, err
